f0d663c52b45e672b71fd9a822818ab9ec5729d1
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-04-01 20:28:35 +0530

    rendered list

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import classes from './MealsSummary.module.css'
+import Card from '../UI/Card';
+import classes from './AvailableMeals.module.css'
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
     {
@@ -29,12 +31,14 @@ const DUMMY_MEALS = [
   ];
 
 const AvailableMeals = () => {
-    const mealsList =DUMMY_MEALS.map(meal=><li>{meal.name}</li>)
+    const mealsList =DUMMY_MEALS.map(meal=><MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>)
   return (
-    <section>
+    <section className={classes.meals}>
+    <Card>
         <ul>
            {mealsList}
         </ul>
+    </Card>
     </section>
   )
 }
