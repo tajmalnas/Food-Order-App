a28fa3966079d3e9a4eb89218ab3b1cfc9158cd3
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-04-09 21:12:20 +0530

    added firebase to store mealsItems

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,37 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css'
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Vada Pav',
-      description: 'Famous fast food',
-      price: 20,
-    },
-    {
-      id: 'm2',
-      name: 'Biryani',
-      description: 'A special dsh!',
-      price: 180,
-    },
-    {
-      id: 'm3',
-      name: 'Burger',
-      description: 'Not from Mcdonalds',
-      price: 35.5,
-    },
-    {
-      id: 'm4',
-      name: 'Paneer Masala',
-      description: 'Healthy   and  Tasty',
-      price: 120,
-    },
-  ];
+// const DUMMY_MEALS = [
+//     {
+//       id: 'm1',
+//       name: 'Vada Pav',
+//       description: 'Famous fast food',
+//       price: 20,
+//     },
+//     {
+//       id: 'm2',
+//       name: 'Biryani',
+//       description: 'A special dish!',
+//       price: 180,
+//     },
+//     {
+//       id: 'm3',
+//       name: 'Burger',
+//       description: 'Not from Mcdonalds',
+//       price: 35.5,
+//     },
+//     {
+//       id: 'm4',
+//       name: 'Paneer Masala',
+//       description: 'Healthy   and  Tasty',
+//       price: 120,
+//     },
+//   ];
+
+
 
 const AvailableMeals = () => {
-    const mealsList =DUMMY_MEALS.map(meal=><MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>)
+
+  const [meals,setMeals] =useState([])
+
+  useEffect(()=>{
+    const fetchMeals=async ()=>{
+      const response = await  fetch('https://food-order-ec663-default-rtdb.firebaseio.com/meals.json')
+
+      const responseData=await response.json();
+
+      const loadedMeals=[];
+
+      for(const key in responseData){
+        loadedMeals.push({
+          id:key,
+          name:responseData[key].name,
+          description:responseData[key].description,
+          price:responseData[key].price
+        })
+      }
+      setMeals(loadedMeals);
+    }
+
+    fetchMeals();
+  },[])
+
+    const mealsList =meals.map(meal=><MealItem id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>)
   return (
     <section className={classes.meals}>
     <Card>
@@ -41,6 +68,6 @@ const AvailableMeals = () => {
     </Card>
     </section>
   )
-}
+}//
 
 export default AvailableMeals
